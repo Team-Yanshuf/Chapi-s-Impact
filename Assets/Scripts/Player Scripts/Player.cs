03c43a2c8c18d8ee405cc29ab17abba56340b1fb
Guy rahamim <46720575+Guy-Rahamim@users.noreply.github.com>
2020-12-28 17:58:14 +0200

    Development (#1)
    Created prefabs and Script templates for:
    -Player, Enemy, Camera, Projectile, UI and Scene

---
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    //Component References.
+    [SerializeField] PlayerMovement movementM;
+    [SerializeField] PlayerAttacking attackM;
+    [SerializeField] PlayerInput inputM;
+    [SerializeField] PlayerHealth healthM;
+
+    [SerializeField] int health;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
