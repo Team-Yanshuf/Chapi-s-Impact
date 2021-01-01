683827c675a579ba02437dd1390a94426e802f46
Guy rahamim <guy.danino5@gmail.com>
2021-01-01 15:24:18 +0200

    Added basic player movement
    Movement axis is shifted according to plane angle.

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -6,16 +6,40 @@ public class PlayerMovement : MonoBehaviour
 {
     //Component References.
     Player playerM;
+    Rigidbody rb;
+    Vector3 movement;
+    [SerializeField] float groundRotation;
+
+    float horizontal, vertical;
 
     [SerializeField] int speed;
     void Start()
-    {
+    { 
         playerM = GetComponent<Player>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        updateAxis();
+        move();
     }
+
+    void updateAxis()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void move()
+    {
+        movement = new Vector3(horizontal, 0, vertical);
+        Quaternion rotation = Quaternion.Euler(groundRotation, 0, 0);
+        movement = rotation * movement;
+        rb.MovePosition(transform.position + movement * speed * Time.deltaTime);
+    }
+
+
+
 }
