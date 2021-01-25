ee81cded1239212146fc81b7186885ac84c87f3a
Guy rahamim <guy.danino5@gmail.com>
2021-01-25 10:47:13 +0200

    Implemented Dash

---
+++ Assets/Scripts/Enemy Scripts/EnemyCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour, IVulnrable
+{
+    OilMonster monsterM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monsterM= GetComponent<OilMonster>();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void takeDamage(int damage=0)
+    {
+
+    }
+}
--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -11,42 +11,85 @@ public class PlayerMovement : MonoBehaviour
 
     float horizontal, vertical;
 
-    [SerializeField] int speed;
+    [SerializeField] float speed;
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashLength;
+    float dashStart;
+    bool isDashing;
+
     void Start()
     { 
         playerM = GetComponent<Player>();
         rb = GetComponent<Rigidbody>();
         renderer = GetComponent<SpriteRenderer>();
+        isDashing = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        setMovementVector();
         move();
 
     }
 
+    private void setMovementVector()
+    {
+        playerM.getMovementAxes(ref horizontal, ref vertical);
+        movement = new Vector3(horizontal, vertical, 0);
+        Quaternion rotation = Quaternion.Euler(groundRotation, 0, 0);
+        movement = rotation * movement;
+    }
 
     void move()
     {
-        playerM.getMovementAxes(ref horizontal, ref vertical);
-        movement = new Vector3(horizontal, vertical,0);
-        Quaternion rotation = Quaternion.Euler(groundRotation, 0, 0);
-        movement = rotation * movement;
-        rb.MovePosition(transform.position + movement * speed * Time.deltaTime);
-    }
 
-    //TODO:change the sorting layer dynamically according to player position.
-    void changeSortingLayer()
-    {
-    }
+        //if (isDashing)
+        //{
+        //    if (dashStart+Time.realtimeSinceStartup>dashLength)
+        //    {
+        //        isDashing = false;
+        //    }
+        //}
 
+        //if (!isMoving())
+            //rb.velocity = Vector3.zero;
+
+         if (Input.GetKeyDown(KeyCode.C))
+        {
+            isDashing = true;
+            Debug.Log("Dashing!");
+            teleDash();
+        }
+
+        else
+        {
+            isDashing = false;
+            rb.MovePosition(transform.position + movement * speed *  Time.deltaTime);
+        }
+    }
 
     public bool isMoving()
     {
         return movement.magnitude != 0;
     }
 
+    public bool isDash()
+    {
+        return isDashing;
+    }
+
+
+    void teleDash()
+    {
+        rb.MovePosition(transform.position + movement * dashLength * Time.deltaTime);
+
+    }
+
 
 
 }
+
+
+
+//TODO:change the sorting layer dynamically according to player position.
