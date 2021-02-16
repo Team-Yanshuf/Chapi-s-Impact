c4ba229f146445bc80cfd60ef0c8524af614a7a2
Guy rahamim <guy.danino5@gmail.com>
2021-02-17 00:03:25 +0200

    -Added state machine behaviour -Added Timer Implementation -Added combo implementation

---
+++ Assets/Scripts/Game Manager/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	GameManager manager;
+	public State currentState;
+
+	void setCurrentState(State state)
+	{
+		currentState.exitState();
+		this.currentState = state;
+		currentState.enterState();
+	}
+	
+	// Start is called before the first frame update
+	private void Awake()=>initGameManagerSingelton();
+	
+
+	// Update is called once per frame
+	void Update()
+	{
+		checkForPause();
+		currentState?.tick();
+	}
+
+	void checkForPause()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			currentState?.exitState();
+			setCurrentState(new GamePauseState());
+		}
+	}
+
+	void initGameManagerSingelton()
+	{
+		if (!manager)
+		{
+			manager = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Game Manager/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public static int currentScene
+    { get; set; }
+
+
+    public  void moveToMainMenu()
+    {
+        SceneManager.LoadScene("StartMenu Experiment");
+    }
+
+    public  void moveToFirstLevel()
+    {
+        SceneManager.LoadScene("Stage001");
+    }
+
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
