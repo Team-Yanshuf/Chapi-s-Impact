9ce796d82bfc37d5f8f6c0f8e68d84266e4ba102
Guy rahamim <guy.danino5@gmail.com>
2021-02-20 14:54:42 +0200

    Fixed Attacking animation parameters

--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -50,6 +50,8 @@ public class Player : MonoBehaviour
     public bool isDashing() => movementM.isDash();
     public bool isCollecting() => collisionM.isCollecting();
     public bool isShooting() => attackM.isShooting();
+
+    public int comboCount() => attackM.getComboCount();
     public void setRendererEnabled(bool enabled) => renderer.enabled = enabled;
 
 
