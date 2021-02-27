027924bbd47289a59aee67f071e6841b21dbf6b6
Guy rahamim <guy.danino5@gmail.com>
2021-02-27 21:48:04 +0200

    Fixed Fog. Started implementing tree planting.

---
+++ Assets/Scripts/Player Scripts/PlayerPlanting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlanting : MonoBehaviour
+{
+    bool planting = false;
+    [SerializeField] Tree tree;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool isPlanting() => planting;
+
+    void plant()
+	{
+        Instantiate(tree, transform.position + new Vector3(5, 0, 0), Quaternion.identity);
+	}
+}
