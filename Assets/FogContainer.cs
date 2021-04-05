38ead89bc90c147eec8a7c9682589d459e9aff37
Guy rahamim <46720575+Guy-Rahamim@users.noreply.github.com>
2021-04-05 21:21:31 +0300

    Fog prefab (#67)
    * Added FogContainer prefab and class.
    
    * Fog is generated correctly.
    
    * Finished the FogContainer prefab!
    
    * Fog now dwindles bit by bit.

---
+++ Assets/FogContainer.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogContainer : MonoBehaviour
+{
+    GameObject particle;
+    FogParticle partic;
+    BoxCollider collider;
+    List<GameObject> pollution;
+    int fogAmount;
+    Transform fogTransform;
+
+    public void setFogTransform(Transform transform)
+    {
+        this.fogTransform = transform;
+    }
+
+    void Start()
+    {
+        particle = Resources.Load<GameObject>("FogParticle/FogParticle");
+        partic = Resources.Load<FogParticle>("FogParticle/FogParticle");
+        if (!partic)
+        {
+            print("partic is null");
+        }
+        if (!particle)
+            print("particle is null");
+        pollution = new List<GameObject>();
+    }
+
+    public void initFog()
+    {
+        StartCoroutine(initFogCoroutine());
+    }
+
+    public IEnumerator initFogCoroutine()
+    {
+        while (partic == null)
+        {
+            yield return null;
+            print("coroutine partic is null");
+        }
+
+        if (fogAmount==0)
+        {
+            print("fog count is zero.");
+            yield break;
+        }
+
+        for (int i=0; i<fogAmount; i++)
+        {
+            Vector3 point = generateRandomPointInsideBounds();
+            FogParticle fog = Instantiate(Resources.Load<FogParticle>("FogPArticle/FogParticle"), point, Quaternion.identity); ;
+            pollution.Add(fog.gameObject);
+        }
+
+    }
+
+    public void setFogCount(int count)
+    {
+        this.fogAmount = count;
+    }
+    
+    public void dwindleByPrecentage(int precent)
+    {
+        StartCoroutine(dwindleFogByPrecentage(precent));
+    }
+    public IEnumerator dwindleFogByPrecentage(int precent)
+    {
+
+        int amount = (pollution.Count * precent) / 100;
+        for (int i=0; i<amount; i++)
+        {
+            Destroy(pollution[0]);
+            pollution.RemoveAt(0);
+            yield return null;
+        }
+
+    }
+
+    public void dwindleByAmout(int amount)
+    {
+        StartCoroutine(dwindleFogByAmount(amount));
+    }
+    public IEnumerator dwindleFogByAmount(int amount)
+    {
+        if (amount>=pollution.Count)
+        {
+            foreach (GameObject particle in pollution)
+            {
+                Destroy(particle);
+            }
+            pollution.Clear();
+        }
+
+        else
+        {
+            for (int i=0; i<amount; i++)
+            {
+                Destroy(pollution[0]);
+                pollution.RemoveAt(0);
+                yield return null;
+            }
+
+
+
+
+        }
+    }
+    public void setBounds(BoxCollider collider)
+    {
+        this.collider = collider;
+    }
+    Vector3 generateRandomPointInsideBounds()
+    {
+        if (collider==null)
+            return Vector3.zero;
+
+        Vector3 point;
+        Bounds bounds = collider.bounds;
+
+        do
+        {
+            point = new Vector3(Random.Range(bounds.min.x, bounds.max.x),
+                                Random.Range(bounds.min.y, bounds.max.y),
+                                Random.Range(bounds.min.z, bounds.max.z));
+            point = fogTransform.TransformPoint(point);
+        }
+        while (point != collider.ClosestPoint(point));
+
+        return point;
+    }
+
+}
--- Assets/Scripts/Game Manager/State Machine/Level States/LevelState.cs
+++ Assets/Scripts/Game Manager/State Machine/Level States/LevelState.cs
@@ -7,20 +7,17 @@ using UnityEngine;
 public abstract class LevelState : State
 {
     int initialFogCount; 
-    FogParticle fogPrefab;
+    FogContainer pollution;
     bool chapiDied = false;
 
     BoxCollider fogBoundries;
     protected LevelManager levelM;
-    int currentFogCount;
 
    protected List<FogParticle> fogs;
-   Vector3[] levelBoundries;
 
    protected Player player;
 
     protected int numberOfEnemies;
-    int fogDeclineRate;
     
     public LevelState()
     { 
@@ -30,35 +27,16 @@ public abstract class LevelState : State
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         levelM = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
         fogBoundries = levelM.GetComponentInChildren<BoxCollider>();
-
-        fogPrefab = Resources.Load<FogParticle>("FogParticle/FogParticle");
      
         initialFogCount = levelM.getInitialFogCount();
 	}
 
     public override void enterState()
 	{
-        levelBoundries = levelM.getLevelBoundries();
         initFog();
-        getRidOfDuplicateFogContiners();
-        initFogList();
-
     }
 
-	private void initFogList()
-	{
-        fogs.AddRange(GameObject.FindGameObjectWithTag("FogContainer").GetComponentsInChildren<FogParticle>());
-	}
-
-	private void getRidOfDuplicateFogContiners()
-	{
-		GameObject[] containers = GameObject.FindGameObjectsWithTag("FogContainer");
-        if (containers.Length>1)
-            for (int i=1; i<containers.Length; i++)
-			{
-                GameObject.Destroy(containers[i].gameObject);
-			}
-	}
+    
 
 	public override void exitState()
 	{
@@ -73,65 +51,24 @@ public abstract class LevelState : State
 
 	void initFog()
 	{
-        fogs = new List<FogParticle>();
-        GameObject fogContainer = new GameObject();
-        fogContainer.tag = "FogContainer";
-        fogContainer.gameObject.name = "Fog Container";
-        for (int i=0; i<initialFogCount; i++)
-		{
-            //float xFront = Random.Range(levelBoundries[0].x, levelBoundries[1].x);
-            //float yFront = Random.Range(levelBoundries[2].y, levelBoundries[0].y);
-            //float zFront = Random.Range(levelBoundries[0].z, levelBoundries[2].z);
-
-            //float xBack = Random.Range(levelBoundries[4].x, levelBoundries[5].x);
-            //float yBack = Random.Range(levelBoundries[6].y, levelBoundries[4].y);
-            //float zBack = Random.Range(levelBoundries[4].z, levelBoundries[6].z);
-
-
-            //Vector3 pos = new Vector3(Random.Range(xBack, xFront), Random.Range(yBack, yFront), Random.Range(zBack, zFront));
-            //Vector3 position = new Vector3(xFront, yFront, zFront);
-            Vector3 pos = getRandomPointInsideFogCollider(fogBoundries.bounds);
-            while (pos != fogBoundries.ClosestPoint(pos))
-                pos = getRandomPointInsideFogCollider(fogBoundries.bounds);
-            FogParticle particle = GameObject.Instantiate(fogPrefab, pos, Quaternion.identity);
-           // fogs.Add(particle);
-           particle.transform.parent = fogContainer.transform;
-		}
-
-     
+        pollution = GameObject.Instantiate(Resources.Load<FogContainer>("FogContainer/FogContainer"));
+        pollution.setBounds(fogBoundries);
+        pollution.setFogCount(initialFogCount);
+        pollution.setFogTransform(levelM.transform.Find("LevelBoundries").transform);
+        pollution.initFog();
 	}
 
 	void clearFogBy20Precent()
 	{
-        int amount = (int) (fogs.Count * 0.2);
-        for (int i = 0; i < amount ; i++)
-        {
-            int indexToRemove = Random.Range(0, currentFogCount);
-            Object.Destroy(fogs[indexToRemove].gameObject);
-            fogs.RemoveAt(indexToRemove);
-        }
-
+        pollution.dwindleByPrecentage(20);
     }
 
     void clear200FogParticles()
 	{
-       for (int i=0; i<fogs.Count && i<200;i++)
-		{
-            int indexToRemove = Random.Range(0, currentFogCount);
-            Object.Destroy(fogs[indexToRemove].gameObject);
-            fogs.RemoveAt(indexToRemove);
-        }
-
-
+        pollution.dwindleByAmout(200);
     }
-    Vector3 getRandomPointInsideFogCollider(Bounds bounds)
-	{
-        Vector3 point = new Vector3(Random.Range(bounds.min.x, bounds.max.x),
-                                    Random.Range(bounds.min.y, bounds.max.y),
-                                    Random.Range(bounds.min.z, bounds.max.z));
-        point = levelM.transform.Find("LevelBoundries").transform.TransformPoint(point);
-        return point;
-	}
+
+
 
 
 
