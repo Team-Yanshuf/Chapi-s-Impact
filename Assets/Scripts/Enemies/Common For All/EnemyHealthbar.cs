ba6ea873f14105ea081c3eb14633e7a6d0e98d20
Guy Rahamim <guy.danino5@gmail.com>
2021-04-14 15:23:52 +0300

    Set up initial healthbar graphics and create the EnemyHealthbar script

---
+++ Assets/Scripts/Enemies/Common For All/EnemyHealthbar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthbar : MonoBehaviour
+{
+    Image healthbar;
+    int maxHealth;
+    int currentHealth;
+    float healthPrecentage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthbar = transform.Find("Foreground").GetComponent<Image>();   
+    }
+
+	// Update is called once per frame
+	private void LateUpdate()
+	{
+		
+	}
+
+
+
+	void setCurrentAndMaxHealth(int maxHealth)
+	{
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+        
+	}
+
+    void updateBar()
+	{
+        healthPrecentage = (float)currentHealth / (float)maxHealth;
+        healthbar.fillAmount = healthPrecentage;
+    }
+
+    void updateHealth()
+}
