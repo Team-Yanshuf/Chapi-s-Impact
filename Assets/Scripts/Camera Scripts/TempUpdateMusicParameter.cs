1263d7d6b0e5dd4a8d90c7017fc5bfb773555e54
Guy Rahamim <guy.danino5@gmail.com>
2021-05-01 21:35:16 +0300

    Added adaptive music using Fmod parameters

---
+++ Assets/Scripts/Camera Scripts/TempUpdateMusicParameter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempUpdateMusicParameter : MonoBehaviour
+{
+    LevelManager levelM;
+    [SerializeField] FMODUnity.StudioEventEmitter emitter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelM = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (levelM.getPollutionInfo().finishedLoading)
+		{
+            float val =  100f / levelM.getPollutionInfo().remainingFogPrecentage- 1f;
+            print(val);
+            emitter.SetParameter("Nature Revive", val);
+        }
+
+    }
+}
