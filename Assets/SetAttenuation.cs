6b2de888c50f9056698229d8157a870a61b352f0
Guy Rahamim <guy.danino5@gmail.com>
2021-05-03 14:05:37 +0300

    Added player attenutation on the camera prefab.

---
+++ Assets/SetAttenuation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(FMODUnity.StudioListener))]
+public class SetAttenuation : MonoBehaviour
+{
+    [SerializeField] FMODUnity.StudioListener listener;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        listener.attenuationObject = player;
+    }
+}
