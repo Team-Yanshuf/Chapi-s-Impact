97d1b49074a5807741b1a53f5cebffb53877b5be
Guy rahamim <46720575+Guy-Rahamim@users.noreply.github.com>
2021-05-15 01:40:52 +0300

    Randomizer (#121)
    * Wrote basic foundation for level randomization.
    
    * Created random map generation.

--- Assets/Scripts/Level Manager/LevelSpawner.cs
+++ Assets/Scripts/Level Manager/LevelSpawner.cs
@@ -4,73 +4,94 @@ using UnityEngine;
 
 public class LevelSpawner : MonoBehaviour
 {
-	//    [SerializeField] GameObject room;
 
-	//    GameObject[,] rooms= new GameObject[10,10];
-	//    int occupied = 5;
-	//    void Start()
-	//    {
-	//        Vector2Int coordinates = new Vector2Int (5, 5);
-	//        for (int i = 0; i<6; i++)
-	//		{
-	//            rooms[coordinates.x, coordinates.y] = Instantiate(room);
-	//            coordinates = chooseNonOccupiedNeighbor(coordinates);
-	//		}
+    [SerializeField] GameObject room;
+	Vector2Int coordinates;
+	GameObject[,] rooms= new GameObject[5,5];
+    int occupied = 5;
+    void Start()
+	{
+		//generateRandomMap();
+		//printMapLayout();
+	}
 
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.G))
+			generateRandomMap();
+	}
+	private void printMapLayout()
+	{
+		for (int i = 0; i < rooms.GetLength(0); i++)
+		{
+			string str = "   ";
+			for (int j = 0; j < rooms.GetLength(1); j++)
+			{
+				if (rooms[i, j] != null)
+					str += "[X]\t";
+				else
+					str += "[ ]\t";
+			}
+			print(str);
+		}
+	}
 
-	//        for(int i=0; i<rooms.GetLength(0); i++)
-	//		{
-	//            for (int j=0; j<rooms.GetLength(1);j++)
-	//			{
-	//                if (rooms[i, j] != null)
-	//                    print($"Room at [{i} , {j}]  exists ");
-	//			}
-	//		}
-	//    }
+	void generateRandomMap()
+	{
+		clearMap();
 
-	//    void Update()
-	//    {
+		coordinates = new Vector2Int(2, 2);
+		for (int i = 0; i < 6; i++)
+		{
+			float offset = 1000;
+			rooms[coordinates.x, coordinates.y] = Instantiate(room, new Vector3(coordinates.x * 200 + offset, coordinates.y * 200, 0), Quaternion.Euler(40,0,0));
+			coordinates = chooseNonOccupiedNeighbor(coordinates);
+		}
+	}
 
-	//    }
-
-	//     Vector2Int chooseNonOccupiedNeighbor(Vector2Int vec)
-	//	{
-	//        int[] fullI = { vec.x - 1, vec.x, vec.x + 1 };
-	//		int[] neighboorI = { vec.x, vec.y };
-
-	//		int[] fullJ = { vec.y - 1, vec.y, vec.y + 1 };
-	//        int[] neighboorJ = { vec.y - 1, vec.y + 1 };
-
-	//        int randI;
-	//        int randJ;
-
-	//        Vector2Int rand;
-	//        do
-	//        {
-	//			//randI = -1;
-	//			//randJ = -1;
-
-
-	//			//         randI = UnityEngine.Random.Range(0, 3);
-
-	//			//         if (randI==1)
-	//			//{
-	//			//             randJ = UnityEngine.Random.Range(0, 2);
-	//			//             rand = new Vector2Int(iNeigh[randI], altJ[randJ]);
-	//			//}
-
-	//			//         else
-	//			//{
-	//			//             randJ = 1;
-	//			//             rand = new Vector2Int(iNeigh[randI], jNeigh[randJ]);
-	//			//}
+	void clearMap()
+	{
+		for(int i=0; i<rooms.GetLength(0); i++)
+		{
+			for (int j=0; j<rooms.GetLength(1); j++)
+			{
+				if (rooms[i, j])
+					Destroy(rooms[i, j]);
+				rooms[i,j] = null;
+			}
+		}
+	}
 
 
-	//			//randI = UnityEngine.Random.Range(0, 3);
-	//			//if (iNeigh[randI] 
-	//		}
-	//        while (fullI[randI]>= 0 && fullI[randI] < 10 && fullJ[randJ] >= 0 && fullJ[randJ] < 10 && rooms[rand.x,rand.y] != null);
 
-	//        return new Vector2Int(fullI[randI], fullJ[randJ]);
-	//	}
+	Vector2Int chooseNonOccupiedNeighbor(Vector2Int vec)
+	{
+        int[] fullI = { vec.x - 1, vec.x, vec.x + 1 };
+		int[] neighboorI = { vec.x, vec.y };
+
+		int[] fullJ = { vec.y - 1, vec.y, vec.y + 1 };
+        int[] neighboorJ = { vec.y - 1, vec.y + 1 };
+		
+        int randI;
+        int randJ;
+
+        Vector2Int rand;
+        do
+        {
+			randI = UnityEngine.Random.Range(0, 3);
+			if (randI==1)
+			{
+				randJ = Random.Range(0, 2);
+				rand = new Vector2Int(vec.x,neighboorJ[randJ]);
+			}
+
+			else
+			{
+				randJ = Random.Range(0, 3);
+				rand = new Vector2Int(fullI[randI], vec.y);
+			}
+		}
+        while (rand.x < 0 || rand.x >= 5 || rand.y < 0 || rand.y >= 5 || rooms[rand.x,rand.y] != null);
+        return rand;
+	}
 }
