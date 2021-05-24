9b6857eb58a306008f9dba4f257cdffc450338f7
Guy Rahamim <guy.danino5@gmail.com>
2021-05-25 01:42:08 +0300

    Started implementing TinyLead Projectile.

---
+++ Assets/Scripts/Projectile Scripts/TinyLeaf.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TinyLeaf : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnCollisionEnter(Collision collision)
+	{
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            IVulnrable enemy = collision.gameObject.GetComponent<IVulnrable>();
+            Vector3 pushback = new Vector3(transform.localScale.x, 0, 0);
+            enemy?.takeDamage(Vector3.zero, 10);
+        }
+    }
+}
