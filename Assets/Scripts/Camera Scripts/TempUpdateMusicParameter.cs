7838c47685a24b985fa54f738535d57191003afa
Guy Rahamim <guy.danino5@gmail.com>
2021-06-03 00:32:12 +0300

    Added dynamic background music support.

--- Assets/Scripts/Camera Scripts/TempUpdateMusicParameter.cs
+++ Assets/Scripts/Camera Scripts/TempUpdateMusicParameter.cs
@@ -3,22 +3,26 @@
 public class TempUpdateMusicParameter : MonoBehaviour
 {
     LevelManager levelM;
-    [SerializeField] FMODUnity.StudioEventEmitter emitter;
+    [SerializeField] FMODUnity.StudioEventEmitter backgroud;
+    [SerializeField] FMODUnity.StudioEventEmitter seagulls;
+    float fogLevel;
     // Start is called before the first frame update
     void Start()
     {
         levelM = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-  //      if (levelM.getRoomManagerInfo().finishedLoading)
-		//{
-  //          float val =  100f / levelM.getRoomManagerInfo().remainingFogPrecentage- 1f;
-  //          //print(val);
-  //          emitter.SetParameter("Nature Revive", val);
-  //      }
+		if (levelM.getRoomManagerInfo().finishedLoading)
+		{
 
-    }
+			float val = 1f-levelM.getRoomManagerInfo().remainingFogPrecentage;
+			backgroud.SetParameter("Nature Revive", val);
+            seagulls.SetParameter("Nature Revive", val);
+		}
+
+	}
 }
