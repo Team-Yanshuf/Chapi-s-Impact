0fe577fa7e1f7a6ac021e666eb7165e0d3a0a246
Guy Rahamim <guy.danino5@gmail.com>
2021-06-07 00:23:36 +0300

    Added Camera Shake! Camera Shake is tightly coupled with WeaponCollision.cs. FIX THIS!

--- Assets/Scripts/Player Scripts/Weapons/General/WeaponCollision.cs
+++ Assets/Scripts/Player Scripts/Weapons/General/WeaponCollision.cs
@@ -6,16 +6,19 @@ public class WeaponCollision : MonoBehaviour
     {
         if (collision.CompareTag("Enemy") && !collision.isTrigger)
         {
+            print("collided with enemy");
             IVulnrable enemy = collision.GetComponent<IVulnrable>();
             Vector3 pushback = new Vector3(transform.localScale.x, 0, 0);
             enemy?.takeDamage(pushback * 5, 25);
+            Camera.main.GetComponent<CameraFollowPlayer>().shake();
         }
 
 		if (collision.CompareTag("ItemTent"))
 		{
 			IVulnrable tent = collision.GetComponent<IVulnrable>();
 			tent.takeDamage(Vector3.zero, 0);
-		}
+            Camera.main.GetComponent<CameraFollowPlayer>().shake();
+        }
 	}
 }
 
