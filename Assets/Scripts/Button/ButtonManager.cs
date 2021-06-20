c51ee490d3df3834a3c2d7a998ada335e1a0e292
Guy rahamim <46720575+Guy-Rahamim@users.noreply.github.com>
2021-06-20 20:22:48 +0300

    Start and quit buttons now work after losing the game. (#185)

--- Assets/Scripts/Button/ButtonManager.cs
+++ Assets/Scripts/Button/ButtonManager.cs
@@ -3,22 +3,23 @@ using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
-    //The point of this class is to find the game manager
-    //during runtime and assign the moving function.
+    ////The point of this class is to find the game manager
+    ////during runtime and assign the moving function.
 
-    GameManager manager;
-    Button button;
+    //GameManager manager;
+    //Button button;
 
-    void Start()
-    {
-        manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
-        button = GetComponent<Button>();
-        button.onClick.RemoveAllListeners();
-        button.onClick.AddListener(manager.moveToFirstLevel);
-    }
+    //void Start()
+    //{
+    //    manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    //    button = GetComponent<Button>();
+    //    button.onClick.RemoveAllListeners();
+    //    button.onClick.AddListener(manager.moveToFirstLevel);
+    //    print(gameObject.name);
+    //}
 
-    void Update()
-    {
+    //void Update()
+    //{
         
-    }
+    //}
 }
---
+++ Assets/StartMenuButtonManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenuButtonManager : MonoBehaviour
+{
+    [SerializeField] Button start;
+    [SerializeField] Button quit;
+
+    GameManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        start.onClick.AddListener(manager.moveToFirstLevel);
+        quit.onClick.AddListener(manager.quit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
