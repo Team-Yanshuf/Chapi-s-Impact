d82b8781752c6597d91ca4d190385bc57c1a7d31
Guy rahamim <46720575+Guy-Rahamim@users.noreply.github.com>
2021-08-01 14:31:58 +0300

    Added Wheel prefab with basic movement. (#191)

---
+++ Assets/Scripts/Enemies/Wheel/Wheel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+    WheelMovement movementM;
+    WheelCollision collisionM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementM = GetComponent<WheelMovement>();
+        movementM.initSelf();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Enemies/Wheel/WheelMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelMovement : MonoBehaviour
+{
+    Wheel wheelM;
+    Vector3 directionVector;
+    [SerializeField] float walkSpeed;
+    [SerializeField] float sprintSpeed;
+    [SerializeField] GameObject target;
+
+    Timer timer;
+    Rigidbody rb;
+    public void initSelf()
+	{
+        wheelM = GetComponent<Wheel>();
+        rb = GetComponent<Rigidbody>();
+        timer = GetComponent<Timer>();
+
+        InvokeRepeating("setWheelSprint",3, 3);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void setMovementVector()
+	{
+        directionVector = (target.transform.position - transform.position).normalized;
+	}
+
+    private void sprint()
+	{
+        print($"Fired! with direction of {directionVector} and speed of {sprintSpeed}");
+        rb.AddForce(directionVector * sprintSpeed, ForceMode.Impulse);
+	}
+    
+
+    private void setWheelSprint()
+	{
+        setMovementVector();
+        sprint();
+	}
+}
