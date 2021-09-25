0062310e13ce00b3440f00a568ad07d89fd63b65
Guy Rahamim <guy.danino5@gmail.com>
2021-09-25 22:14:43 +0300

    Deleted unneccecery prefabs.

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -4,20 +4,21 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    int health;
+    int currentHealth;
     Player player;
     Image image;
     float interpolator;
     Vector3 target;
     Vector3 initial;
+
     
     public void InitSelf()
 	{
         interpolator = 0f;
 
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-        health = player.getHP();
-        print(health);
+        currentHealth = player.getHP();
+        print(currentHealth);
         image = GetComponentsInChildren<Image>()[2];
         initial = image.rectTransform.localPosition;
         target = new Vector3(initial.x, initial.y - 4f, initial.z);
@@ -30,20 +31,23 @@ public class HealthBar : MonoBehaviour
 
 	private void triggerFlaskReduction()
 	{
-        if (health != player?.getHP())
+        if (currentHealth != player?.getHP())
             StartCoroutine(updateFlask());
 	}
 
     IEnumerator updateFlask()
 	{
-        int maxHealth = player.GetPlayerHealthInfo().maxHP;
-        int difference = health - (int)player?.getHP();
-        health = (int)player?.getHP();
-        for (int i=0; i<difference; i++)
-		{
-            interpolator += 1f / maxHealth;
-            image.rectTransform.localPosition = Vector3.Lerp(initial, target, interpolator);
+        PlayerHealthInfo info = player.GetPlayerHealthInfo();
+        int maxHealth = info.maxHP;
+        int currentHealthFromPlayer = info.currentHP;
 
+        int differenceFromPreviousAmountOfHealth = currentHealth - currentHealthFromPlayer;
+        currentHealth = currentHealthFromPlayer;
+
+        for (int i=0; i<differenceFromPreviousAmountOfHealth; i++)
+		{
+            image.rectTransform.localPosition = Vector3.Lerp(initial, target, interpolator);
+            interpolator += 1f / maxHealth;
             yield return null;
 		}
 
